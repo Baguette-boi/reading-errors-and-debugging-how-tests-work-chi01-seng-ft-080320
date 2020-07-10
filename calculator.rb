0a299b85717d60937665fa4aba9931bcfd4a839b
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-10 02:42:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,5 @@ first_number = 2
 second_number = 4
 sum = first_number + second_number
 difference = first_number - second_number
-first_number * second_number
-second_number / first_number
+product = first_number * second_number
+quotient = second_number / first_number
